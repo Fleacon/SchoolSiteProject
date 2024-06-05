4e246d3c0ee4df5c310111b65d33dd08a14009db
Fleacon <david.noskov0603@gmail.com>
2024-06-05 22:36:15 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var dropdown = document.getElementsByClassName("dropdownbutton");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = document.getElementsByClassName("dropdownitems");
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
